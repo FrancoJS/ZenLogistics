3c87a58fc01f55e18717220760f10fc667e760a1
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-20 11:37:31 -0300

    feat(core-api): Define user entity properties
    Implement UserEntity extending AbstractEntity. Relations are pending

--- apps/core-api/src/users/entities/user.entity.ts
+++ apps/core-api/src/users/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { AbstractEntity } from '@app/common';
+import { Column, Entity } from 'typeorm';
+
+export enum UserRole {
+  CLIENT = 'client',
+  DRIVER = 'driver',
+  ADMIN = 'admin',
+}
+
+export enum AuthProvider {
+  EMAIL = 'email',
+  GOOGLE = 'google',
+}
+
+@Entity('users')
+export class User extends AbstractEntity {
+  @Column({ type: 'varchar', unique: true, length: 255 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, select: false, nullable: true })
+  password: string;
+
+  @Column({ type: 'varchar', length: 150 })
+  full_name: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  phone: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.CLIENT })
+  role: UserRole;
+
+  @Column({ type: 'enum', enum: AuthProvider, default: AuthProvider.EMAIL })
+  authProvider: AuthProvider;
+
+  @Column({ type: 'varchar', nullable: true, select: false })
+  googleId: string;
+
+  @Column({ default: false })
+  isEmailVerified: boolean;
+
+  // Campo adicional para vincular user con driver profile si es el caso
+}
