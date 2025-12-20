6356298e439034c8c7d7eaac325f53c3369df6c0
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-19 21:05:26 -0300

    feat(core-api): configure database connection and env validation

--- apps/core-api/src/core-api.module.ts
+++ apps/core-api/src/core-api.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { CoreApiController } from './core-api.controller';
 import { CoreApiService } from './core-api.service';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [CoreApiController],
   providers: [CoreApiService],
 })
--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -5,4 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(CoreApiModule);
   await app.listen(process.env.port ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Core API is running...');
+  })
+  .catch((err) => {
+    console.error('Error starting Core API:', err);
+  });
