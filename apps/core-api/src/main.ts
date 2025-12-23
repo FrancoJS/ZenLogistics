f5a2b47b35d8e8e9fdc4dfa4d8ae9c48683486a4
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-22 22:25:33 -0300

    feat(core-api): setup global validation pipe
    Enable ValidationPipe globally with whitelist, forbidNonWhiteListed and transform options

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { CoreApiModule } from './core-api.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(CoreApiModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap()
