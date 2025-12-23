0652ff96e88df0f5a58b441d3ddbb7052d1adc86
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-22 22:32:12 -0300

    feat(core-api): implement user creation logic in service

--- apps/core-api/src/users/users.service.ts
+++ apps/core-api/src/users/users.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateClientDto } from './dto/create-client.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { UserRole } from './enums/auth-provider.enum';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
+  async createClient(createClientDto: CreateClientDto) {
+    const user = this.userRepository.create({
+      ...createClientDto,
+      authProvider: createClientDto.authProvider,
+      role: UserRole.CLIENT,
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return await this.userRepository.save(user);
   }
 }
