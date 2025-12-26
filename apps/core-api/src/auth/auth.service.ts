9f0ef48dc0aec691086b0b2c9f236bbb6299752d
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-26 18:44:29 -0300

    feat(core-api): implement jwt generation on registration
    Create AuthService method to sign tokens using JwtService. Define JwtPayload interface

--- apps/core-api/src/auth/auth.service.ts
+++ apps/core-api/src/auth/auth.service.ts
@@ -3,22 +3,53 @@ import { UsersService } from '../users/users.service';
 import { RegisterClientDto } from './dto/register-client.dto';
 import { RegisterDriverDto } from './dto/register-driver.dto';
 import { AuthProvider } from '../users/enums/auth-provider.enum';
+import { JwtService } from '@nestjs/jwt';
+import { IJwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  private getJwtToken(payload: IJwtPayload) {
+    return this.jwtService.sign(payload);
+  }
 
   async registerClient(registerClientDto: RegisterClientDto) {
-    return await this.userService.createClient({
+    const user = await this.userService.createClient({
       ...registerClientDto,
       authProvider: AuthProvider.LOCAL,
     });
+
+    const token = this.getJwtToken({
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 
   async registerDriver(registerDriverDto: RegisterDriverDto) {
-    return await this.userService.createDriver({
+    const user = await this.userService.createDriver({
       ...registerDriverDto,
       authProvider: AuthProvider.LOCAL,
     });
+
+    const token = this.getJwtToken({
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
