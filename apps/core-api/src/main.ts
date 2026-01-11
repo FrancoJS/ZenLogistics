1cd540af6dad5d44593403a4f4c6cd781cb8eba8
Franco_SJ <jean.jara.fs@gmail.com>
2026-01-11 12:30:12 -0300

    feat(core-api): configure Swagger documentation setup in main.ts

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { CoreApiModule } from './core-api.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
 import { DbExceptionFilter } from '@app/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(CoreApiModule);
+  const logger = new Logger('Bootstrap');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,7 +24,24 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.port ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Zen Logistics API')
+    .setDescription(
+      'Plataforma SaaS B2B para gestion de lógistica y transporte.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+
+  logger.log(`Aplicación corriendo en el puerto ${process.env.PORT}`);
+  logger.log(
+    `Documentación disponible en http://localhost:${process.env.PORT}/docs`,
+  );
 }
 bootstrap()
   .then(() => {
